cf2d950ebf1959d83ccf6994f5e07fc0196f74ff
Daniel <danielstarling4@gmail.com>
2019-04-17 15:11:55 -0700

    fixed DS api Post route, cleaned the object
    Co-authored-by: Daniel Starling <danielstarliing4@gmail.com>
    Co-authored-by: Dan Sherman <dvsherman@protonmail.com>

--- src/middleware/generateToken.js
+++ src/middleware/generateToken.js
@@ -2,8 +2,6 @@ require('dotenv').config();
 const jwt = require('jsonwebtoken');
 
 module.exports = function generateToken(username, id) {
-  // console.log(username);
-  // console.log(id);
   const payload = {
     subject: id,
     username: username
